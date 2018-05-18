8ad513d081d708593e64340237b07e4486d023d9
usmannisar <usman.nisar@gmail.com>
2018-05-18 15:02:50 -0700

    jjnn

--- testing/lambda.js
+++ testing/lambda.js
@@ -2,5 +2,5 @@ let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	callback(null,'Successfully ejjjjxecutejkkkd');
 }
