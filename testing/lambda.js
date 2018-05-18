aace7ff7216a9098b1e803081ebbf315b03b1c1a
usmannisar <usman.nisar@gmail.com>
2018-05-18 14:48:43 -0700

    jjjnnn

--- testing/lambda.js
+++ testing/lambda.js
@@ -1,6 +1,8 @@
 let AWS = require('aws-sdk');
+let ask = require('ask-sdk');
+
 exports.handler = function(event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	callback(null,'Successfully executed, or maybe not');
 }
